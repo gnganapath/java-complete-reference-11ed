c7ee86ef897d2d12c4dc0eb41eba54ed6c933710
gnganpath <esourcej@gmail.com>
2020-05-27 06:34:23 +0530

    java the complete ref 11 ed - Begins

---
+++ Example.java
@@ -0,0 +1,9 @@
+/* 
+This is just simple java program
+*/
+class Example{
+	//Program begis with call to main()
+	public static void main(String[] args){
+		System.out.print("Hello Java world");
+	}
+}	
