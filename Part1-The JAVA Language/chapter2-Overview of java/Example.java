6bac633d1a465a4ee3eaaf40681b8e71965f2e91
gnganapath <gnganapathi@gmail.com>
2021-11-14 09:39:45 +0530

    new workspace from hpavilion

--- Part1-The JAVA Language/chapter2-Overview of java/Example.java
+++ Part1-The JAVA Language/chapter2-Overview of java/Example.java
@@ -4,7 +4,7 @@ This is just simple java program
 class Example{
 	//Program begins with call to main()
 	public static void main(String[] args){
-		System.out.print("Hello Java world");
+		System.out.print("Hello Java world, JAVA 11 Edition");
 	}
 }	
 
